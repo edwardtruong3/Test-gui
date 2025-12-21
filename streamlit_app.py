957b24f6ddb629658b49cdcab9ddadb2eaa9122d
edwardtruong3 <23364936@student.uwa.edu.au>
2025-12-21 09:24:28 +0000

    New title/subtitle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("AI predictor")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Machine learning risk prediction of AF recurrence within 12 months post-catheter ablation."
 )
